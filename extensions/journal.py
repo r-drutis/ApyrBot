bf0d29f528b47a7173ebc3ba8a17e6f0ae2f9063
r-drutis <robertdrutis@gmail.com>
2022-06-25 13:15:53 -0400

    Refactoring - Increasing code readability

--- extensions/journal.py
+++ extensions/journal.py
@@ -2,29 +2,42 @@ import hikari
 import lightbulb
 from webscraper import retrieve_query_from_journal
 from lightbulb.utils.pag import StringPaginator
-from extensions.journal_view import FrogNavButton
+from journal_view import FrogNavButton
 from miru.ext import nav
 
-fun_plugin = lightbulb.Plugin("Fun")
+journal_plugin = lightbulb.Plugin("Journal")
 
-@fun_plugin.command
+@journal_plugin.command
 @lightbulb.command("apyr", "Tell me what you want to know, and I will see what I can find...")
 @lightbulb.implements(lightbulb.SlashCommandGroup)
-async def fun_group(ctx: lightbulb.Context) -> None:
+async def apyr_group(ctx: lightbulb.Context) -> None:
     pass  # as slash commands cannot have their top-level command ran, we simply pass here
 
-@fun_group.child
+"""
+Search Roll20 External Journal
+
+This command accepts a search term and then navigates to the roll20 external journal and retrieves the journal entry 
+for that search term.
+
+The journal entry is returned as a paginated discord embed with page navigation buttons and thumbnail image.
+
+If no entry is found, it responds and tells the user nothing was found.
+"""
+
+@apyr_group.child
 @lightbulb.option('term', 'The term you want me to search for')
 @lightbulb.command("what-is", "Search for something in the roll20 journal")
 @lightbulb.implements(lightbulb.SlashSubCommand)
-async def meme_subcommand(ctx: lightbulb.Context) -> None:
+async def whatis_subcommand(ctx: lightbulb.Context) -> None:
     await ctx.respond("You want to know what \"{0}\" is? Let me check the archives for you...".format(ctx.options.term))
     info = retrieve_query_from_journal(ctx.options.term)
     if info:
         await ctx.respond("Here is what I found:\n")
+        # Build the embed and navigator view
         pages = build_embed_list(info, ctx)
         buttons = [nav.FirstButton(), nav.PrevButton(), FrogNavButton(), nav.NextButton(),nav.LastButton(), nav.IndicatorButton()]
         navigator = nav.NavigatorView(pages=pages, buttons=buttons)
+        # Send the navigator view to the discord channel
         await navigator.send(ctx.channel_id)
     else:
         await ctx.respond("I wasn't able to find anything by that name. Ribbit.")
@@ -59,4 +72,4 @@ def build_embed(info, ctx):
     return embed
 
 def load(bot: lightbulb.BotApp) -> None:
-    bot.add_plugin(fun_plugin)
+    bot.add_plugin(journal_plugin)
--- webscraper.py
+++ webscraper.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from extensions.journal_exceptions import UnknownEntryTypeError
+from journal_exceptions import UnknownEntryTypeError
 import os
 from dotenv import load_dotenv
 
